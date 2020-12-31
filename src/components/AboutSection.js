306e3ab040f6a19c8252080edc98e61da816ff8c
arielvergarautn <arie@est.frba.utn.edu.ar>
2020-12-31 07:39:40 -0300

    about us animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,29 +2,32 @@ import React from 'react'
 import home1 from '../img/home1.png'
 //Framer motion
 import { motion } from 'framer-motion'
-
+import { titleAnim, fade, photoAnim } from '../animation'
+import {
+    Link
+} from 'react-router-dom'
 
 const AboutSection = () => {
 
     return (
-        <div className='about layout'>
-            <div className="description">
-                <motion.div className="title" initial='hidden' animate='show'>
+        <div id='about' className=' layout'>
+            < div className="description" >
+                <motion.div className="title">
                     <div className="hide">
-                        <motion.h2 >We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </div>
                     <div className="hide">
-                        <motion.h2  >your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </div>
                     <div className="hide">
-                        <motion.h2  >true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </div>
                 </motion.div>
-                <p>Contact us for any fotography or videography ideas that you have. We have professional with amazing skills</p>
-                <button>Contact Us</button>
-            </div>
+                <motion.p variants={fade}>Contact us for any fotography or videography ideas that you have. We have professional with amazing skills</motion.p>
+                <motion.button variants={fade}><Link to='/contact'>Contact Us</Link></motion.button>
+            </div >
             <div className="image">
-                <img src={home1} alt="Guy with a camera" />
+                <motion.img variants={photoAnim} src={home1} alt="A guy with a camera" />
             </div>
 
         </div >
